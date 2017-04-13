d868f5c15993868cd8a0b2a3b117985daa7a0649
Xavier Nouvilas <xavier.nouvilas@matmax.es>
2017-04-13 10:35:14 +0200

    Change grid test adding a unit when content is a map

--- test/zf/grid_test.exs
+++ test/zf/grid_test.exs
@@ -5,7 +5,8 @@ defmodule Zf.GridTest do
   setup_all do
     {:ok,
       grid_default: [[content: "Some content", class: "small-12"], [content: "Some content", class: "small-12"]],
-      grid_custom: [[tag: :li, content: "Some content", class: "small-12"], [tag: :li, content: "Some content", class: "small-12"]]
+      grid_custom: [[tag: :li, content: "Some content", class: "small-12"], [tag: :li, content: "Some content", class: "small-12"]],
+      grid_map: [%{content: "Some content", class: "small-12"}, %{content: "Some content", class: "small-12"}],
     }
   end
 
@@ -32,6 +33,19 @@ defmodule Zf.GridTest do
     }
 
     assert expected == Zf.Grid.create([class: "expanded align-middle", tag: :ul], params[:grid_custom])
+
+  end
+
+  test "spawn foundation grid when content is a list of maps", params do
+
+    expected = {:safe, [60, "div", [[32, "class", 61, 34, "expanded align-middle row", 34]], 62,
+      [[60, "div", [[32, "class", 61, 34, "small-12 columns", 34]], 62,
+        "Some content", 60, 47, "div", 62],
+      [60, "div", [[32, "class", 61, 34, "small-12 columns", 34]], 62,
+        "Some content", 60, 47, "div", 62]], 60, 47, "div", 62]
+    }
+
+    assert expected == Zf.Grid.create([class: "expanded align-middle"], params[:grid_map])
 
   end
 end
